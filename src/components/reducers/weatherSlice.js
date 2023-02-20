801f6d9156aa6c531d8fa841e8093d49ce4dc036
wayungi <fwayungi@gmail.com>
2023-02-20 20:02:40 +0300

    ADD: extrareducer

--- src/components/reducers/weatherSlice.js
+++ src/components/reducers/weatherSlice.js
@@ -1,16 +1,38 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
-const initialState = [];
+// https://www.weatherapi.com/
+const baseUrl = 'https://api.weatherapi.com/v1/current.json?key=81c94cbab5f34936b61145833232002&q=kampala&aqi=yes';
+const initialState = {
+  status: 'idle',
+  data: {},
+  error: null,
+};
+
+export const fetchWeather = createAsyncThunk('weather/fetchWeather', async () => {
+  const response = await axios.get(baseUrl);
+  const result = response.data;
+  return result;
+});
 
 export const weatherSlice = createSlice({
   name: 'weather',
   initialState,
   reducers: {},
-
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchWeather.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchWeather.fulfilled, (state, action) => {
+        state.status = 'fulfilled';
+        return action.payload;
+      })
+      .addCase(fetchWeather.rejected, (state) => {
+        state.status = 'rejected';
+      });
+  },
 });
-
-// Action creators are generated for each case reducer function
-export const { increment, decrement, incrementByAmount } = weatherSlice.actions;
 
 export default weatherSlice.reducer;
