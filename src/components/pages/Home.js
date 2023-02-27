c8ae57afe6837388ffb0ec715680e72c48d5433e
wayungi <fwayungi@gmail.com>
2023-02-27 23:25:51 +0300

    FIX: undefine property value when chaining objects

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -20,11 +20,38 @@ const Home = () => {
 
   console.log(data);
 
+  const formattedWeatherData = (
+    <article>
+      <div className="region">
+        <h2>{data.tz_id}</h2>
+      </div>
+      <div>
+        <span>
+          {data.feelslike_c}
+          <sup>o</sup>
+          C
+        </span>
+        /
+        <span>
+          {data.feelslike_f}
+          <sup>o</sup>
+          F
+        </span>
+      </div>
+      <div>
+        <div>
+          <img src={`https:${data?.condition?.icon}`} alt="weather" />
+        </div>
+      </div>
+    </article>
+  );
+
   return (
     <section className="container">
       <header>
         <h1>Real time Weather App</h1>
       </header>
+      {formattedWeatherData}
       {/* <div>
         <div className="region">
           <h2>{items}</h2>
