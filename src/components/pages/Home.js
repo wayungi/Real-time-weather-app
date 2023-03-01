30242c66b33c6608bf8c6660f618a249d19b5107
wayungi <fwayungi@gmail.com>
2023-03-01 23:02:37 +0300

    ADD:search form

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React, { useEffect, useState } from 'react';
-import { BsSearch } from 'react-icons/bs';
-import Modal from 'react-overlays/Modal';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchWeather, queryStatus } from '../reducers/weatherSlice';
 // import Search from '../partials/Search';
@@ -11,8 +9,7 @@ const Home = () => {
   const weatherData = useSelector((state) => state.weather.data);
   const queryState = useSelector(queryStatus);
   // const displaySearch = useSelector(canSearch);
-  const [showModal, setShowModal] = useState(false);
-
+  const [location, setLocation] = useState('kampala');
   useEffect(() => {
     if (queryState === 'idle') {
       dispatch(fetchWeather());
@@ -24,99 +21,86 @@ const Home = () => {
     ...weatherData.current,
   };
 
-  const handleClose = () => setShowModal(false);
-
-  const handleSave = () => {
-    console.log('success');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLocation(e.target.search.value);
   };
 
-  const renderBackdrop = () => <div className="backdrop" />;
+  console.log(location);
 
-    <Modal
-      className="modal"
-      show={showModal}
-      onHide={handleClose}
-      renderBackdrop={renderBackdrop}
-    >
+  const formattedWeatherData = (
+    <article>
+      <div className="region">
+        <h2>
+          {data.tz_id?.split('/')[0]}
+          {' '}
+          /
+          {' '}
+          {data.country}
+        </h2>
+        <h3>{data.name}</h3>
+      </div>
+
       <div>
-        <form>
-          <input type="search" name="location" placeholder="Search" />
-          <input type="submit" value="Search" onClick={handleSave} />
+        <div>
+          <img src={`https:${data?.condition?.icon}`} alt="weather" />
+        </div>
+        <p>{data?.condition?.text}</p>
+      </div>
+
+      <div>
+        <div>
+          Temperature
+          <span>
+            {data.feelslike_c}
+            <sup>o</sup>
+            C
+          </span>
+          /
+          <span>
+            {data.feelslike_f}
+            <sup>o</sup>
+            F
+          </span>
+        </div>
+        <p>
+          Wind speed
+          {' '}
+          <span>{data.wind_kph}</span>
+          kph
+          {' '}
+          /
+          <span>{data.wind_mph}</span>
+          mph
+        </p>
+        <p>
+          Wind direction
+          {' '}
+          <span>{data.wind_dir}</span>
+        </p>
+        <p>
+          Pressure
+          {' '}
+          <span>{data.pressure_in}</span>
+          in /
+          <span>{data.pressure_mb}</span>
+          mb
+        </p>
+      </div>
+
+    </article>
+  );
+
+  return (
+    <section className="container">
+      <div>
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <input type="text" className="search-field" name="search" placeholder="Enter location" />
         </form>
       </div>
-    </Modal>;
-
-    const formattedWeatherData = (
-      <article>
-        <div className="region">
-          <h2>
-            {data.tz_id?.split('/')[0]}
-            {' '}
-            /
-            {' '}
-            {data.country}
-          </h2>
-          <h3>{data.name}</h3>
-        </div>
-
-        <div>
-          <div>
-            <img src={`https:${data?.condition?.icon}`} alt="weather" />
-          </div>
-          <p>{data?.condition?.text}</p>
-        </div>
-
-        <div>
-          <div>
-            Temperature
-            <span>
-              {data.feelslike_c}
-              <sup>o</sup>
-              C
-            </span>
-            /
-            <span>
-              {data.feelslike_f}
-              <sup>o</sup>
-              F
-            </span>
-          </div>
-          <p>
-            Wind speed
-            {' '}
-            <span>{data.wind_kph}</span>
-            kph
-            {' '}
-            /
-            <span>{data.wind_mph}</span>
-            mph
-          </p>
-          <p>
-            Wind direction
-            {' '}
-            <span>{data.wind_dir}</span>
-          </p>
-          <p>
-            Pressure
-            {' '}
-            <span>{data.pressure_in}</span>
-            in /
-            <span>{data.pressure_mb}</span>
-            mb
-          </p>
-        </div>
-
-      </article>
-    );
-
-    return (
-      <section className="container">
-        <div>
-          <BsSearch onClick={() => setShowModal(true)} />
-        </div>
-        {formattedWeatherData }
-      </section>
-    );
+      {formattedWeatherData }
+    </section>
+  );
 };
 
 export default Home;
