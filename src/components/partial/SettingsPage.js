1ae03813c9ceaf88551d2f5b6df0ed9830272ea4
wayungi <fwayungi@gmail.com>
2023-03-22 22:22:06 +0300

    ADD: styles to windspeed radio buttons

--- src/components/partial/SettingsPage.js
+++ src/components/partial/SettingsPage.js
@@ -3,23 +3,42 @@ import React from 'react';
 const SettingsPage = () => (
   <form className="absolute inset-0 bg-orange-300 ">
     <section className="m-7 rounded p-5 bg-teal-400">
-      <div>
-        <input type="text" placeholder="Enter default location" name="location" />
+      <div className="mb-5">
+        <input
+          type="text"
+          placeholder="Enter default location"
+          name="location"
+          className="p-3 rounded focus:outline-none"
+        />
       </div>
-      <div>
-        <p>Wind Speed Units</p>
-        <label htmlFor="wind_speed">
+
+      <div className="mb-5">
+        <p className="mb-2">Wind Speed Units</p>
+        <label htmlFor="wind_speed" className="pl-3">
           kph
-          <input type="radio" id="wind" name="wind_speed" value="kph" />
+          <input
+            type="radio"
+            id="wind"
+            name="wind_speed"
+            value="kph"
+            className="mr-7 ml-2"
+            checked
+          />
         </label>
 
-        <label htmlFor="wind_speed">
+        <label htmlFor="wind_speed" className="pl-3">
           mph
-          <input type="radio" id="wind" name="wind_speed" value="mph" />
+          <input
+            type="radio"
+            id="wind"
+            name="wind_speed"
+            value="mph"
+            className="ml-2"
+          />
         </label>
       </div>
 
-      <div>
+      <div className="mb-5">
         <p>Pressure</p>
         <label htmlFor="pressure">
           millibars
@@ -32,7 +51,7 @@ const SettingsPage = () => (
         </label>
       </div>
 
-      <div>
+      <div className="mb-5">
         <p>Temperature</p>
         <label htmlFor="temperature">
           Celcius
